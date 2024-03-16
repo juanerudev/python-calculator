798357bfe0e2c767c9e34a21972714e1080b6af2
Juan Rodriguez <juanestebanrodriguezurrea@gmail.com>
2024-03-16 18:15:20 -0500

    Primer commit, se hacen las 4 operaciones básicas y se muestra su resultado

---
+++ calculator.py
@@ -0,0 +1,12 @@
+number_one = int(input("Ingrese un número: "))
+number_two = int(input("Ingrese un número: "))
+
+suma = number_one + number_two
+resta = number_one - number_two
+multiplicacion = number_one * number_two
+division = number_one / number_two
+
+print(f"{number_one} + {number_two} = {suma}")
+print(f"{number_one} - {number_two} = {resta}")
+print(f"{number_one} * {number_two} = {multiplicacion}")
+print(f"{number_one} / {number_two} = {division}")
